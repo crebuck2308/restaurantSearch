566845b4a91442c070c7f5c0e941471deee7e46a
Chris Rebuck <crebuck.apt@gmail.com>
2021-07-25 22:42:07 -0500

    Initial Commit, set up createStackNavigation and SearchScreen.

---
+++ src/SearchScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+
+    return (
+        <Text>Search Screen</Text>
+    );
+};
+
+const styles=StyleSheet.create({ });
+
+export default SearchScreen;
