5a77b67458f53e514da71d35ee05c9f4a27f82e7
Chris Rebuck <crebuck.apt@gmail.com>
2021-07-26 23:11:35 -0500

    Created ResultsDetail component to reuse for ResultsList to display each restaurant and appropriate information on screen

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
+import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ( { title, results }) => {
 
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.titleStyle}>{title}</Text>
             <FlatList 
                 horizontal
+                showsHorizontalScrollIndicator={false}
                 data={results}
-                keyExtractor={result => results.id}
+                keyExtractor={results => results.id}
                 renderItem={({ item }) => {
-                    return <Text>{item.name}</Text>;
+                    return <ResultsDetail result={item}/>;
                 }}
             />
         </View>
@@ -19,9 +21,14 @@ const ResultsList = ( { title, results }) => {
 };
 
 const styles=StyleSheet.create({ 
+    container: {
+        marginBottom: 10
+    },
     titleStyle: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
     }
 });
 
