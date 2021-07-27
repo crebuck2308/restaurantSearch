8a5e261466c635530aadbb8489eb300466b6714d
Chris Rebuck <crebuck.apt@gmail.com>
2021-07-26 22:16:28 -0500

    Created a ResultsList component to help render search results on SearchScreen.

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.yelp.com/v3/businesses",
     headers: {
-        Authorization: 'Bearer puHrJ9ENa2wKz11jRollgZfHU_CvMr9-hF4IbhfW90P-mBEEqLrrhwFa4WGoVzvK9LI7FF2EbgUyWg57e4S0P0RC4zW37EWNUxtOlCpMdF5xuKgSlYtqj1KQ6kD0YHYx'
+        Authorization: 'Bearer '
     }
 });
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 
 const SearchScreen = () => {
@@ -17,6 +18,11 @@ const SearchScreen = () => {
             />
             {errorMessage ? <Text style={styles.errorStyle}>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
+
+            <ResultsList title="Cost Effective"/>
+            <ResultsList title="A Bit Pricer"/>
+            <ResultsList title="A Tad Expensive"/>
+            <ResultsList title="Big Spender" />
         </View>
     );
 };
